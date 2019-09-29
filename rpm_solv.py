099f97b4b4b975822deb5eda8e3d8fda7f0138b5
h0neybadger <h0neyb4dgerdc@gmail.com>
2019-09-29 23:25:36 +0200

    Add REFERENCE infos to advisorie data

--- rpm_solv.py
+++ rpm_solv.py
@@ -16,6 +16,8 @@ import configparser
 import re
 import json
 
+from collections import OrderedDict
+
 from utils.repo import dir_path, \
         repo_repomd, \
         load_stub, \
@@ -52,6 +54,7 @@ def add_solvable_to_stack(solvable, data):
     str_name = solvable.lookup_str(solv.SOLVABLE_NAME)
     str_arch = solvable.lookup_str(solv.SOLVABLE_ARCH)
     str_patchcategory = solvable.lookup_str(solv.SOLVABLE_PATCHCATEGORY)
+
     str_severity = solvable.lookup_str(solv.UPDATE_SEVERITY)
     str_reboot = solvable.lookup_str(solv.UPDATE_REBOOT)
     num_buildtime = solvable.lookup_num(solv.SOLVABLE_BUILDTIME)
@@ -66,15 +69,37 @@ def add_solvable_to_stack(solvable, data):
     if not other or solvable.evrcmp(other) == 1:
         d['solvable'] = solvable
 
+    # read reference
+    pack = solvable.Dataiterator(solv.UPDATE_REFERENCE_ID, '*', solv.Dataiterator.SEARCH_GLOB)
+    pack.prepend_keyname(solv.UPDATE_REFERENCE)
+    references = []
+    for p in pack: 
+        pos = p.parentpos()
+        str_reference_id = pos.lookup_str(solv.UPDATE_REFERENCE_ID)
+        str_reference_title = pos.lookup_str(solv.UPDATE_REFERENCE_TITLE)
+        str_reference_href = pos.lookup_str(solv.UPDATE_REFERENCE_HREF)
+        str_reference_type = pos.lookup_str(solv.UPDATE_REFERENCE_TYPE)
+        references.append(
+            OrderedDict((
+                ("references", str_reference_id),
+                ("reference_title", str_reference_title),
+                ("reference_href", str_reference_href),
+                ("reference_type", str_reference_type),
+            ))
+        )
+
     # read UPDATE_COLLECTION to add advisories packages
     # to the solver process 
     pack = solvable.Dataiterator(solv.UPDATE_COLLECTION_NAME, '*', solv.Dataiterator.SEARCH_GLOB)
     pack.prepend_keyname(solv.UPDATE_COLLECTION)
+    col_ids = []
     for p in pack:
         pos = p.parentpos()
         str_col_evr = pos.lookup_str(solv.UPDATE_COLLECTION_EVR)
         str_col_name = pos.lookup_str(solv.UPDATE_COLLECTION_NAME)
         str_col_arch = pos.lookup_str(solv.UPDATE_COLLECTION_ARCH)
+        #str_col_filename = pos.lookup_str(solv.UPDATE_COLLECTION_FILENAME)
+        #col_flags = pos.lookup_str(solv.UPDATE_COLLECTION_FLAGS)
         #str_sev = pos.lookup_str(solv.UPDATE_SEVERITY)
         nevra = "{}-{}.{}".format(str_col_name, str_col_evr, str_col_arch)
         sel = solvable.pool.select(nevra, solv.Selection.SELECTION_DOTARCH|solv.Selection.SELECTION_CANON)
@@ -89,13 +114,18 @@ def add_solvable_to_stack(solvable, data):
             advisories = d.get('advisories',[])
             d['advisories'] = advisories
             
-            adv = {
-                "name": str_name,
-                "severity": str_severity,
-                "patchcategory": str_patchcategory,
-                "buildtime": num_buildtime,
-            }
-            advisories.append(adv)
+            names = [o['name'] for o in advisories] 
+            if str_name not in names: 
+                # keep a list of unique advisories
+                adv = OrderedDict((
+                    ("name", str_name),
+                    ("severity", str_severity),
+                    ("patchcategory", str_patchcategory),
+                    ("buildtime", num_buildtime),
+                    ("references", references),
+                ))
+                
+                advisories.append(adv)
             add_solvable_to_stack(cs, data)
     print('')
 
@@ -314,14 +344,15 @@ def main():
             nevra = "{}-{}.{}".format(str_name, str_evr, str_arch)
             # update or insert errata in packages list
             na = "{}.{}".format(str_name, str_arch)
-            d = data.get(na,{})
-            data[na] = d
-            d['nevra'] = nevra
-            d['name'] = str_name
-            d['evr'] = str_evr
-            d['arch'] = str_arch
-            d['repo'] = str(p.repo) 
-            d['buildtime'] = num_buildtime
+            old_data = data.get(na,{})
+            d = OrderedDict((
+                ('nevra', nevra),
+                ('name', str_name),
+                ('evr', str_evr),
+                ('arch', str_arch),
+                ('repo', str(p.repo)),
+                ('buildtime', num_buildtime),
+            ))
             # 1:3.0.12-17.el7
             ma = evr_re.match(str_evr)
             if ma is not None: 
@@ -338,7 +369,10 @@ def main():
             else: 
                 frmt_str = '{epoch}:{name}-{version}.{arch}'
             d['envra'] = frmt_str.format(**d)
-
+            adv = old_data.get('advisories', None)
+            if adv is not None:
+                d['advisories'] = old_data.get('advisories', [])
+            data[na] = d
             if cl.type == solv.Transaction.SOLVER_TRANSACTION_UPGRADED or cl.type == solv.Transaction.SOLVER_TRANSACTION_DOWNGRADED:
                 op = trans.othersolvable(p)
                 print("  - %s -> %s" % (p, op))
@@ -359,7 +393,11 @@ def main():
         s = val.pop('job', None)
 
     with open('{}/data.json'.format(args.exportdir), 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
+        # sort data's packages name 
+        # the sort is just to ease human reading 
+        # and/or diff comparison
+        odata = OrderedDict(sorted(data.items()))
+        json.dump(odata, f, ensure_ascii=False, indent=4)
 
 if __name__== "__main__":
     main()
